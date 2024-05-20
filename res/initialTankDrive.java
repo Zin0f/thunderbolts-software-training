7359a3faf3e2fafd9e8588820074ff796a13f1d0
adiaviad <adaviad4@gmail.com>
2024-05-20 22:15:40 +0300

    added a while to the opmodes

--- res/initialTankDrive.java
+++ res/initialTankDrive.java
@@ -2,6 +2,10 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import static com.qualcomm.robotcore.hardware.DcMotor.RunMode.*;
+import static com.qualcomm.robotcore.hardware.DcMotorSimple.Direction.*;
+
 
 @TeleOp(name = "tank drive")
 public class TankDrive extends LinearOpMode {
@@ -9,7 +13,9 @@ public class TankDrive extends LinearOpMode {
     public void runOpMode() throws InterruptedException {
         //קוד אתחול צריך להיות כאן
         waitForStart();//אחרי אתחול נחכה שנלחץ על כפתור ההפעלה
-        
+        while (opModeIsActive()) {
+            
+        }
 
     }
 }
