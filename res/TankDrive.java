9e33b39b3e36e5d6a74a65c5f6fae68b0dd0bf49
adiaviad <adaviad4@gmail.com>
2024-05-20 14:48:32 +0300

    initial code state

---
+++ res/TankDrive.java
@@ -0,0 +1,17 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name = "tank drive")
+public class TankDrive extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        //קוד אתחול צריך להיות כאן
+        waitForStart();//אחרי אתחול נחכה שנלחץ על כפתור ההפעלה
+        while (opModeIsActive()){
+            //קוד תפעול צריך להיות כאן
+        }
+
+    }
+}
