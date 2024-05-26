86a7c957bd9bd38f2980916b6d934f635ecc9a43
adiaviad <adaviad4@gmail.com>
2024-05-26 17:16:33 +0300

    updated code and added image

--- res/final-code/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drive.java
+++ res/final-code/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drive.java
@@ -51,14 +51,15 @@ public class Drive extends LinearOpMode {
             y=gamepad1.left_stick_y;
             x=gamepad1.left_stick_x;
 
+            speed_left=y-x;
+            speed_right=y+x;
+
+            if(slow_mode) {
+                speed_right = speed_right / parameters.speed_reduction;
+                speed_left = speed_left / parameters.speed_reduction;
+            }
+
             if(Math.abs(y)>0.1 || Math.abs(x)>0.1){
-
-                speed_left=y-x;
-                speed_right=y+x;
-
-                speed_right=speed_right/parameters.speed_reduction;
-                speed_left=speed_left/parameters.speed_reduction;
-
                 left_motor.setPower(speed_left);
                 right_motor.setPower(speed_right);
             }
