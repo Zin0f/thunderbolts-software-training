b2aa640e100a4128bd12ec4ee4472144aa354b2a
adiaviad <adaviad4@gmail.com>
2024-05-27 20:26:37 +0300

    wip

---
+++ res/secondTankDriveWithSlow.java
@@ -0,0 +1,49 @@
+package org.firstinspires.ftc.teamcode;
+
+import static com.qualcomm.robotcore.hardware.DcMotor.RunMode.*;
+import static com.qualcomm.robotcore.hardware.DcMotorSimple.Direction.*;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.acmerobotics.dashboard.FtcDashboard;
+@TeleOp(name = "tank drive")
+public class TankDrive extends LinearOpMode {
+    @Override
+    public void runOpMode()  {
+        Telemetry dashboard=FtcDashboard.getInstance().getTelemetry();
+
+        DcMotor left_motor;
+        DcMotor right_motor;
+        float speed_left;
+        float speed_right;
+
+        left_motor = hardwareMap.dcMotor.get("1");
+        right_motor = hardwareMap.dcMotor.get("2");
+
+        left_motor.setMode(RUN_WITHOUT_ENCODER);
+        right_motor.setMode(RUN_WITHOUT_ENCODER);
+
+        left_motor.setDirection(FORWARD);
+        right_motor.setDirection(REVERSE);
+
+        waitForStart();
+        while (opModeIsActive()){
+            speed_left=-gamepad1.left_stick_y;
+            speed_right=gamepad1.right_stick_y;
+            
+            if(gamepad1.a){ //add this
+                speed_left=speed_left/2;
+                speed_right=speed_right/2;
+            }
+
+            left_motor.setPower(speed_left);
+            right_motor.setPower(speed_right);
+
+            dashboard.addData("left speed",speed_left);
+            dashboard.addData("right speed",speed_right);
+            dashboard.update();
+        }
+
+    }
+}
