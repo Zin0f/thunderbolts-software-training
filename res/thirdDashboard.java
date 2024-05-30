aa7bca2e06256e809c0ecf2fd395ed393ded5f78
adiaviad <adaviad4@gmail.com>
2024-05-30 19:58:34 +0300

    added dashboard example

---
+++ res/thirdDashboard.java
@@ -0,0 +1,47 @@
+package org.firstinspires.ftc.teamcode;
+
+import static com.qualcomm.robotcore.hardware.DcMotor.RunMode.*;
+import static com.qualcomm.robotcore.hardware.DcMotorSimple.Direction.*;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp(name = "tank drive")
+public class TankDrive extends LinearOpMode {
+    @Override
+    public void runOpMode()  {
+
+        DcMotor left_motor;
+        DcMotor right_motor;
+        float speed_left;
+        float speed_right;
+        boolean slow_robot;
+        
+        left_motor = hardwareMap.dcMotor.get("1");
+        right_motor = hardwareMap.dcMotor.get("2");
+
+        left_motor.setMode(RUN_WITHOUT_ENCODER);
+        right_motor.setMode(RUN_WITHOUT_ENCODER);
+
+        left_motor.setDirection(FORWARD);
+        right_motor.setDirection(REVERSE);
+
+        waitForStart();
+        while (opModeIsActive()){
+            speed_left=-gamepad1.left_stick_y;
+            speed_right=gamepad1.right_stick_y;
+            slow_robot=gamepad1.a;
+       
+            if(slow_robot){
+                speed_left=speed_left/2;
+                speed_right=speed_right/2;
+            }
+            
+
+            left_motor.setPower(speed_left);
+            right_motor.setPower(speed_right);
+        }
+
+    }
+}
